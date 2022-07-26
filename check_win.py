86255d19dbf6eb848ea454ae6c8dfd332a87b972
RodovR <rodovroman@yandex.ru>
2022-07-26 22:18:26 +0300

    Добавлен новый файл check_win.py     Проверка ряда на выигрышную комбинацию

--- check_win.py
+++ check_win.py
@@ -1,8 +1,12 @@
 from field import show_field
 
 # FIXME: не забудьте аннотировать и документировать функцию
-def check_win():
-    pass
+def check_win()->list:
+    # Проверяет каждый ряд на выигрышную комбинацию
+    for i in range(len(show_field())):
+        if show_field()[0] and show_field()[1] and show_field()[2] == 'X' or 'O':
+            print('You Win')
+        else:
+            print('You Lose')
 
-
-field_from_checkWin = show_field()
+check_win()
