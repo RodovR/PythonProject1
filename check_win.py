6778b7f80e65c51bc8f51294774222b1c0cb93d3
RodovR <rodovroman@yandex.ru>
2022-07-26 14:02:54 +0300

    Добавлен новый файл field.py     Вывод игрового поля

---
+++ check_win.py
@@ -0,0 +1,6 @@
+from field import show_field
+def check_win():
+    pass
+
+
+field_from_checkWin = show_field()
---
+++ field.py
@@ -0,0 +1,11 @@
+
+def show_field():
+    field = [ [' ', ' ', ' '],
+              [' ', ' ', ' '],
+              [' ', ' ', ' '] ]
+    players = ('player 1', 'player 2')
+    symbol = ('X', 'O')
+    for i in range(len(field)):
+        print(field[i])
+
+show_field()
