3178f48c3db7e8753707a0b641eff30ec078b763
RodovR <rodovroman@yandex.ru>
2022-07-27 14:26:57 +0300

    Добавлен файл get_difficultly_level.py     import PLAYERS, bot1, bot2     Смоделирована заготовка для выбора сложности   игры

---
+++ get_difficultly_level.py
@@ -0,0 +1,10 @@
+from main import PLAYERS
+# Заготовка на будущее для импортирования ботов с разной сложностью игры
+#from easy_mode improt bot1(bot1 - easy mode)
+#from hard_mode improt bot2((bot2 - hard mode))
+
+def get_difficultly_level(level):
+    if level == 'easy':
+        #bot1()
+    if level == 'hard':
+        # bot2()
